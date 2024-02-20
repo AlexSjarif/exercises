2bec8991140e8f1466801f82f29887f949766dc3
Sjarif <alex.sjarif@student.ucll.be>
2024-02-20 11:04:39 +0100

    made first 3

--- 01-basic-python/01-arithmetic/03-cakes/student.py
+++ 01-basic-python/01-arithmetic/03-cakes/student.py
@@ -1,1 +1,9 @@
 # Write your code here
+def cakes(eggs, butter, flour):
+    ei = eggs//5
+    boter = butter//250
+    bloem = flour //250
+    return min(ei,boter,bloem)
+
+print(cakes(18,600,1000))
+# cakes(40,400,1000)
